1349165a488578afef23f65c4f16cb0e55337964
dev22419 <dgr22419@gmail.com>
2022-11-29 19:37:02 +0530

    29/11/

--- pro/python/a4/11.py
+++ pro/python/a4/11.py
@@ -1,6 +1,6 @@
 year=int(input("enter a year:"))
 if(year%400==0)and(year%100==0):
-    print("{0} is a leap year".format(year))
+        print("{0} is a leap year".format(year))
 elif(year%4==0)and(year%100!=0):
         print("{0} is a leap year".format(year))
 else:
--- pro/python/a7/19.py
+++ pro/python/a7/19.py
@@ -1,16 +1,16 @@
 # code by : dev patel
 # https://www.github.com/dev22419/
 
-sum = 0;
+sum = 0
 N = 5
 for i in range(1, N + 1) :
 
     if (i & 1) :
-        sum += i / (i + 1);
+        sum += i / (i + 1)
     
     else :
-        sum -= i / (i + 1);
+        sum -= i / (i + 1)
     
 
 
-print(sum);
+print(sum)
